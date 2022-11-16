c12f127b298dc84610b3f58e59afaac28ae92a4c
Craig <cdm21d@fsu.edu>
2022-11-16 13:06:26 -0500

    Got multiple videos populated

--- sketch.js
+++ sketch.js
@@ -1,11 +1,45 @@
-let width = 400;
-let height = 400;
+// let width = 400;
+// let height = 400;
+
+let vid;
+let vid2;
+
+let isplaying;
+let isPressed;
+
 
 function setup() {
-  createCanvas(width, height);
+  noCanvas();
+  vid = createVideo(['t1.mp4'], vidLoad);
+  vid2 = createVideo(['t2.mp4'], vidLoad);
+  vid.size(400, AUTO);
+  vid2.size(200,AUTO);
+  vid.position(100, 0);
+  vid2.position(50, 150);
 }
 
-function draw() {
-  background(125);
-  text("This is the Project 4 canvas", width/2, height/2);
+function vidLoad() {
+  vid.loop()
+
+  vid2.loop()
+
+  // vid.play()
+
+  // vid2.play()
+
+  isPlaying() = true;
 }
+
+
+// function draw() {
+//   background(125);
+//   text("This is the Project 4 canvas", width/2, height/2);
+// }
+
+// function mouseClicked(){
+//   if (mouseIsPressed) {
+//     isPressed = true
+//   } else {
+//     isPressed = false
+//   }
+// }
